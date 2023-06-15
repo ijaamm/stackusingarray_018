4f827d3474cf21c8cd85a3a2a61abdd9adaf4d0f
ijaamm <ijammusyaf@gmail.com>
2023-06-15 11:00:53 +0700

    case 1

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -67,6 +67,13 @@ int main() {
 		getline(cin, input);
 		char ch = (input.empty() ? '0' : input[0]);
 		switch (ch) {
+		case '1': {
+			cout << "\nEnter an Element: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
 
 		}
 	}
